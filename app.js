80e511f357edbf34aefd5bafa7106c45ad0681aa
TomatoSoup <jonathantang0126@gmail.com>
2023-03-28 23:22:08 +0800

    feat: 🎸 add line webhook route

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import line from '@line/bot-sdk'
+
+dotenv.config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const lineConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+}
+
+app.post('/webhook', line.middleware(lineConfig), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
 })
+
+const client = new line.Client(lineConfig)
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  })
+}
 
 const port = process.env.PORT || 3000
 
